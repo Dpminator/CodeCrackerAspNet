5d38b11d33a5524c9a09221087baecc0d54587fd
Dominic Romeo <dominic4010@hotmail.com>
2020-05-10 13:35:52 +1000

    It works!

--- CodeCracker/Pages/ImportMaker.cshtml.cs
+++ CodeCracker/Pages/ImportMaker.cshtml.cs
@@ -1,25 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CodeCracker.Core;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CodeCracker.Pages
 {
     public class ImportMakerModel : PageModel
     {
-        public string lines = "dfsfggfdggfgsfd";
-
         public void OnGet()
         {
-        }
 
-        public void OnPost()
-        {
-            //new PuzzleSolver(lines).SolveCodeCracker();
-            System.Console.WriteLine("We did it!");
         }
     }
 }
