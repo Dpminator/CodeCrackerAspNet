bdb4674404d75f1832de82c51eedcbe3921c6e04
Dominic Romeo <dominic4010@hotmail.com>
2020-05-22 23:33:58 +1000

    More stuff

--- CodeCracker/Pages/ImportMaker.cshtml.cs
+++ CodeCracker/Pages/ImportMaker.cshtml.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CodeCracker.Pages
 {
     public class ImportMakerModel : PageModel
     {
+        public int GridHeight = 19;
+        public int GridWidth = 15;
+
         public void OnGet()
         {
+            var query = Request.Query;
 
+            if (query.ContainsKey("height")) GridHeight = int.Parse(query["height"].First());
+            if (query.ContainsKey("width")) GridWidth = int.Parse(query["width"].First());
         }
     }
 }
